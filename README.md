d1389a5db0fe5769971bc1b83191d11e6a902111
Kesse Dias <kesse.dias97@outlook.com>
2021-10-07 23:25:42 -0300

    :sparkles: feat: Subindo estrutura inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+<h1 align="center"> Remodelagem - ISD GESSO </h1>
+
+
+## Reforação do projeto já existente ⭐️ 
+> https://github.com/kessedias/ISDGesso
+
+> Em breve novas features
+
+ 
---
+++ index.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <!-- Meta tags Obrigatórias -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+    integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/css/styles.css">
+  <title>ISD GESSO</title>
+</head>
+
+<body>
+
+  <!-- MENU -->
+  <header>
+    <div class="row menu">
+      <div class="col pr-0">
+        <p>menu</p>
+      </div>
+    </div>
+    <div class="row carrousel">
+      <div class="col pr-0">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100"
+                src="https://www.jornalspnorte.com.br/wp-content/uploads/2016/10/banner-1200x300.png"
+                alt="Primeiro Slide">
+            </div>
+
+            <div class="carousel-item">
+              <img class="d-block w-100"
+                src="https://www.jornalspnorte.com.br/wp-content/uploads/2016/10/banner-1200x300.png"
+                alt="Segundo Slide">
+            </div>
+
+            <div class="carousel-item">
+              <img class="d-block w-100"
+                src="https://www.jornalspnorte.com.br/wp-content/uploads/2016/10/banner-1200x300.png"
+                alt="Terceiro Slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+          </a>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- SERVIÇOS -->
+  <section class="container">
+    <h2 class="text-center mt-1 mb-1">Serviços</h2>
+    <article class="row services">
+      <div class="col">
+        serviço 1
+      </div>
+    </article>
+    <article class="row services">
+      <div class="col">
+        serviço 2
+      </div>
+    </article>
+    <article class="row services">
+      <div class="col">
+        serviço 3
+      </div>
+    </article>
+    <article class="row services">
+      <div class="col">
+        serviço 4
+      </div>
+    </article>
+  </section>
+
+  <!-- PORTFÓLIO -->
+  <section>
+    <h2 class="text-center mt-1 mb-1">Portfólio</h2>
+    <article class="row jobs">
+
+      <div class="col">
+        <div id="carouseljobs" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouseljobs" data-slide-to="0" class="active"></li>
+            <li data-target="#carouseljobs" data-slide-to="1"></li>
+            <li data-target="#carouseljobs" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="https://www.jornalspnorte.com.br/wp-content/uploads/2016/10/banner-1200x300.png" alt="Segundo Slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>JOB 1</h5>
+                <p>DESCRIÇÃO 1</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://www.jornalspnorte.com.br/wp-content/uploads/2016/10/banner-1200x300.png" alt="Segundo Slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>JOB 2</h5>
+                <p>DESCRIÇÃO 2</p>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="https://www.jornalspnorte.com.br/wp-content/uploads/2016/10/banner-1200x300.png" alt="Terceiro Slide">
+              <div class="carousel-caption d-none d-md-block">
+                <h5>JOB 3</h5>
+                <p>DESCRIÇÃO 3</p>
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouseljobs" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Anterior</span>
+          </a>
+          <a class="carousel-control-next" href="#carouseljobs" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Próximo</span>
+          </a>
+        </div>
+      </div>
+
+    </article>
+
+  </section>
+
+  <!-- SOBRE NÓS -->
+  <section class="container">
+    <h2 class="text-center mt-1 mb-1">Sobre nós</h2>
+    <article class="row about">
+      <div class="col">
+         Historinha da empresa
+      </div>
+    </article>
+  </section>
+
+  <!-- CONTATO -->
+  <section>
+    <h2 class="text-center mt-1 mb-1">Contato</h2>
+    <article class="row contact p-5">
+      <div class="col-6" style="background-color: white; height: 400px;">
+      </div> 
+      <div class="col-6" style="background-color: gray; height: 400px;">
+      </div>
+    </article>
+  </section>
+
+  <!-- RODAPÉ -->
+  <footer>
+    <div class="row footer">
+      <div class="col pr-0">
+        <p>footer</p>
+      </div>
+    </div>
+
+  </footer>
+  <!-- JavaScript (Opcional) -->
+  <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+    crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+    integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+    crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+    integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+    crossorigin="anonymous"></script>
+</body>
+
+</html>
